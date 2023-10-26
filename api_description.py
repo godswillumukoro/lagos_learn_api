b2a7de062e3784a4fefb7a7c106cc9f62fda8596
Godswill Umukoro <umukro6@gmail.com>
2023-10-26 11:40:04 +0100

    structure api setup

---
+++ api_description.py
@@ -0,0 +1,15 @@
+description = """
+LagosLearn API is engineered to streamline the management of educational resources, enabling easy integration of diverse learning materials, such as courses, modules, assessments, and multimedia content. It empowers educators to create engaging and personalized learning experiences, fostering dynamic interactions and knowledge dissemination among students.
+
+The API offers a range of features tailored to meet the specific needs of educators, students, and institutions within the bustling educational landscape of Lagos, Nigeria. Its user-friendly interface and robust functionality make it an ideal solution for educational organizations seeking to optimize their learning processes.
+## Items
+
+You can **read items**.
+
+## Users
+
+You will be able to:
+
+* **Create users** (_not implemented_).
+* **Read users** (_not implemented_).
+"""
